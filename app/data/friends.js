4b22d7bba7f4bef81351b047a75e230b6c48d89b
María Seneca <blackopal@protonmail.ch>
2018-03-06 18:20:56 -0500

    test

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 
 var friendsArray = [
   {
-    name: "Joe",
+    name: "Donald Trump",
     photo: "https://robohash.org/autdistinctioqui.png?size=100x100&set=set1",
     scores: [5, 4, 3, 2, 1, 2, 4, 5, 1, 4]
   },
   {
-    name: "Auria",
+    name: "Rick Gates",
     photo: "https://robohash.org/suntsaepenecessitatibus.jpg?size=100x100&set=set1",
     scores: [4, 4, 3, 2, 1, 5, 3, 5, 1, 2]
   },
   {
-    name: "Ina",
+    name: "Hope Hicks",
     photo: "https://robohash.org/consequaturetratione.jpg?size=100x100&set=set1",
     scores: [1, 3, 5, 2, 1, 2, 4, 1, 3, 2]
   }
